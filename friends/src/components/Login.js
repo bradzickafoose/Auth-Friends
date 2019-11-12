4893cdef6b78ae08839e85ea591f1a2cfb78de78
Brad <brad@viziworx.com>
2019-11-12 17:02:26 -0500

    Setup initial login component

---
+++ friends/src/components/Login.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import axios from 'axios';
+
+class Login extends React.Component {
+    state = {
+        credentials: {
+            username: "",
+            password: ""
+        },
+        isLoggedIn: false
+    }
+
+
+}
+
+export default Login
