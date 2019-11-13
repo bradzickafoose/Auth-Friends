084787c783aa1cb91f2c58c4b94fb7f1ef3319a8
Brad <brad@viziworx.com>
2019-11-13 14:27:57 -0500

    Aded a Private Route component

--- friends/src/components/App.js
+++ friends/src/components/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect, Switch, Link } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
 import Login from '../routes/Login';
+import PrivateRoute from '../components/PrivateRoute';
 
 
 function App() {
@@ -21,8 +22,12 @@ function App() {
             </li>
           </ul>
           <Switch>
-            <Route path='/login' component={Login} />
-            <Route component={Login} />
+            <Route path='/login'>
+              {localStorage.getItem('token') ? <Redirect to='/' /> : <Login />}
+            </Route>
+            <PrivateRoute path='/'>
+              <h1>Friends</h1>
+            </PrivateRoute>
           </Switch>
         </section>
       </div>
