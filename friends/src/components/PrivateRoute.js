b7d969acc9c4af184e485f771605ffbc83ce598e
Brad <brad@viziworx.com>
2020-05-01 01:17:48 -0400

    adds authentication to prop handling

--- friends/src/components/PrivateRoute.js
+++ friends/src/components/PrivateRoute.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 
-const PrivateRoute = ({ children, ...rest }) => {
+const PrivateRoute = ({ isAuthenticated, children, ...rest }) => {
     return (
         <Route
             {...rest}
             render={({ location }) =>
-                localStorage.getItem('token') ? (children) : (<Redirect to={{ pathname: '/login', state: { from: location }, }} />)} />
+                isAuthenticated ? (children) : (<Redirect to={{ pathname: '/login', state: { from: location }, }} />)} />
     )
 }
 
