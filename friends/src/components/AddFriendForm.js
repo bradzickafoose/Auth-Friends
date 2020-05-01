ba7178de1e5ffc5b5860469ae4ffe82ec25424b2
Brad <brad@viziworx.com>
2020-05-01 13:09:46 -0400

    adds styling to form

--- friends/src/components/AddFriendForm.js
+++ friends/src/components/AddFriendForm.js
@@ -31,30 +31,43 @@ const AddFriendForm = props => {
     }
 
     return (
-        <form onSubmit={onSubmit}>
+      <div className="w-full max-w-xs mx-auto mt-6 pt-4">
+        <h2 class=" text-center text-3xl leading-9 font-extrabold text-gray-900">Add a Friend
+        </h2>
+        <form onSubmit={onSubmit} className="px-8 pt-6 pb-6 mb-4">
+        <div class="mb-4">
             <input
                 type='text'
                 name='name'
                 value={friend.name}
                 placeholder='Name'
                 onChange={handleChange}
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             />
+        </div>
+        <div class="mb-4">
             <input
                 type='text'
                 name='age'
                 value={friend.age}
                 placeholder='Age'
                 onChange={handleChange}
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             />
+      </div>
+      <div class="mb-4">
             <input
-                type='text'
+                type='email'
                 name='email'
                 value={friend.email}
-                placeholder='email'
+                placeholder='Email'
                 onChange={handleChange}
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             />
-            <button type='submit'>Add Friend</button>
+    </div>
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type='submit'>Add Friend</button>
         </form>
+        </div>
     )
 }
 
