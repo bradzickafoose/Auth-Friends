c3b6c3949a28e55d6e7c9e50224c755b14658f5d
Brad <brad@viziworx.com>
2020-05-01 13:10:09 -0400

    adds styling to friend component

--- friends/src/components/Friend.js
+++ friends/src/components/Friend.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 const Friend = ({ name, age, email }) => {
     return (
-      <>
-        <h3>{name}</h3>
+      <div className="bg-white shadow overflow-hidden sm:rounded-lg p-4">
+        <h3 className="text-sm leading-5 font-medium text-gray-900">{name}</h3>
         <div className='Friend-info'>
-            <span>{age}</span>
-            <span>{email}</span>
+            <div className="text-sm leading-5 text-gray-500">{age}</div>
+            <div className="text-sm leading-5 text-gray-500">{email}</div>
         </div>
-      </>
+      </div>
     )
 }
 
